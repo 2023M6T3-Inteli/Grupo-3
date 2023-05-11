c0cb6e184cf3beb7668898c74306f58a7ca93a2b
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2023-05-11 15:03:10 -0300

    centralização - livia

--- codigo/frontend/src/pages/Profile/Profile.tsx
+++ codigo/frontend/src/pages/Profile/Profile.tsx
@@ -1,22 +1,95 @@
-import {
-  Container,
-  ProfileInfo,
-  BottonsProfile,
-  Badges,
-  Imagem,
-} from "./style";
+// import {  Container,  ProfileInfo,  BottonsProfile,  Badges,  Imagem,} from "./style";
+// import Button from "@mui/material/Button";
+// import styled from "styled-components";
+// import Selena from "../../assets/Selena_Gomez_at_White_House.jpg";
+// import { Box } from "@mui/material";
+
+// var StyledButton = styled(Button)`
+//   max-width: 400px;
+//   width: 100%;
+//   // border-radius: 50%;
+// `;
+
+// var StyledImage = styled.div`
+//   width: 100px;
+//   height: 100px;
+//   border-radius: 50%;
+//   background-image: url(${Selena});
+//   background-size: cover;
+//   justify-content: center;
+//   margin-left: auto;
+//   margin-right: auto;
+// `;
+
+// // var ImagemFrufru = styled.img`
+// //   width: 100%;
+// //   heigth: 100%;
+// // `;
+
+// function Profile() {
+//   return (
+//     <Container>
+//       <ProfileInfo>
+//         <StyledImage></StyledImage>
+//         <h1>Clara Alemão</h1> {/* name */}
+//         <p>Front-end developer</p> {/* role of the user */}
+//         <p>São Paulo - SP</p> {/* local */}
+//       </ProfileInfo>
+//       <BottonsProfile>
+//         <StyledButton variant="contained" color="primary">
+//           {" "}
+//           View Dell Curriculum{" "}
+//         </StyledButton>
+//         <br></br>
+//         <br></br>
+
+//         <StyledButton variant="contained" color="primary">
+//           {" "}
+//           Call in Teams{" "}
+//         </StyledButton>
+//       </BottonsProfile>
+//       <Badges>
+//         <h4>You`re level 10</h4>
+//         <div>
+//           <div id="blue background"></div>
+//           <div id="gray blackground"></div>
+//           <h6>Achievements</h6>
+//           <div>
+//             <img></img>
+//             <img></img>
+//             <img></img>
+//             <img></img>
+//           </div>
+//         </div>
+//       </Badges>
+//     </Container>
+//   );
+// }
+
+// export default Profile;
+
+////////////////////////////
+//////////////////////////
+// MANTIVE ACIMA O CÓDIGO ORIGINAL DO GABRIEL //
+///////////////////////////////
+/////////////////////////////////
+
+
+import {Container,ProfileInfo,BottonsProfile,Badges,Imagem, } from "./style";
 import Button from "@mui/material/Button";
 import styled from "styled-components";
 import Selena from "../../assets/Selena_Gomez_at_White_House.jpg";
 import { Box } from "@mui/material";
 
+//estilização do botão - também usamos material U.I. aqui
 var StyledButton = styled(Button)`
   max-width: 400px;
   width: 100%;
   // border-radius: 50%;
 `;
 
-var ImagemEstilizada = styled.div`
+//estilização referente à imagem
+var StyledImage = styled.div`
   width: 100px;
   height: 100px;
   border-radius: 50%;
@@ -24,24 +97,38 @@ var ImagemEstilizada = styled.div`
   background-size: cover;
   justify-content: center;
   margin-left: auto;
-  margin-right: auto; 
+  margin-right: auto;
 `;
 
-// var ImagemFrufru = styled.img`
-//   width: 100%;
-//   heigth: 100%;
-// `;
+//informações do perfil
+var StyledProfileInfo = styled(ProfileInfo)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+//botões do perfil
+var StyledBottonsProfile = styled(BottonsProfile)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 16px;
+`;
 
 function Profile() {
   return (
     <Container>
-      <ProfileInfo>
-        <ImagemEstilizada></ImagemEstilizada>
-        <h1>Clara Alemão</h1> {/* name */}
-        <p>Front-end developer</p> {/* role of the user */}
+      <StyledProfileInfo>
+        <StyledImage></StyledImage>
+        <h1>Selena Gomez</h1> {/* name */}
+        <p>A singer that wanna be a front-end developer</p> {/* role of the user */}
         <p>São Paulo - SP</p> {/* local */}
-      </ProfileInfo>
-      <BottonsProfile>
+      </StyledProfileInfo>
+
+      <StyledBottonsProfile>
         <StyledButton variant="contained" color="primary">
           {" "}
           View Dell Curriculum{" "}
@@ -53,18 +140,21 @@ function Profile() {
           {" "}
           Call in Teams{" "}
         </StyledButton>
-      </BottonsProfile>
+      </StyledBottonsProfile>
+
       <Badges>
-        <h4>You`re level 10</h4>
         <div>
-          <div id="blue background"></div>
-          <div id="gray blackground"></div>
-          <h6>Achievements</h6>
+          <h4>You`re level 10</h4>
           <div>
-            <img></img>
-            <img></img>
-            <img></img>
-            <img></img>
+            {/* <div id="blue background"></div>
+            <div id="gray blackground"></div> */}
+            <h6>Achievements</h6>
+            {/* <div>
+              <img></img>
+              <img></img>
+              <img></img>
+              <img></img>
+            </div> */}
           </div>
         </div>
       </Badges>
@@ -73,3 +163,5 @@ function Profile() {
 }
 
 export default Profile;
+
+/////////////////
