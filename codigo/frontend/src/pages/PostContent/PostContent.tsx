09c9eae6180347bb57c17a7f9e4b8a0c084d838a
matheusmacedosantos <matheus.santos@sou.inteli.edu.br>
2023-05-25 17:32:24 -0300

    finalização da pagina de post com titulo

--- codigo/frontend/src/pages/PostContent/PostContent.tsx
+++ codigo/frontend/src/pages/PostContent/PostContent.tsx
@@ -1,7 +1,37 @@
 import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import TagsInput from './Components/tagsinput';
+import { CancelButton, ContainerHead, InputContainer, InputTextarea, InputTitle, PersonContainer, PostButton, Privacy, ProfileImage, ProfileImageContainer, ProfileInfo, ProfileName } from './styles';
 
-const UserProfile: React.FC = () => {
+const PostContent: React.FC = () => {
+  const [postData, setPostData] = useState({
+    content: '',
+    image: '',
+    tags: [] as string[],
+  });
+
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  // const selectedTags = tags => console.log(tags)};
+
+  const handlePost = () => {
+    axios
+      .post('/api/posts', postData)
+      .then((response) => {
+        console.log('Post criado com sucesso:', response.data);
+        setPostData({
+          content: '',
+          image: '',
+          tags: [],
+        });
+        setSelectedTags([]);
+      })
+      .catch((error) => {
+        console.error('Ocorreu um erro ao criar o post:', error);
+      });
+  };
+
   const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
 
   useEffect(() => {
@@ -19,93 +49,8 @@ const UserProfile: React.FC = () => {
     fetchUserProfile();
   }, []);
 
-  const ContainerHead = styled.div`
-    display: flex;
-    height: 4rem;
-    width: 100%;
-    align-items: center;
-    justify-content: center;
-  `;
 
-  const CancelButton = styled.button`
-    position: absolute;
-    top: 1rem;
-    left: 1rem;
-    margin: 0;
-    font-size: 1rem;
-    color: #0063b8;
-    margin-top: 1rem;
-    margin-left: 1rem;
-  `;
-
-  const PostButton = styled.button`
-    position: absolute;
-    top: 1rem;
-    right: 1rem;
-    margin: 0;
-    font-size: 1rem;
-    color: #fff;
-    border-radius: 20px;
-    background-color: #0063b8;
-    margin-top: 1rem;
-    margin-right: 1rem;
-    padding: 0.34rem 1rem;
-  `;
-
-  const InputContainer = styled.div`
-    width: 95%;
-    margin: auto;
-  `;
-
-  const InputTextarea = styled.textarea`
-    width: 100%;
-    padding: 12px;
-    border: none;
-    border-radius: 8px;
-    resize: vertical;
-    font-size: 16px;
-    outline: none;
-    overflow: hidden;
-    rows: 1;
-  `;
-
-  const PersonContainer = styled.div`
-    height: 4rem;
-    width: 100%;
-    display: flex;
-    align-items: center;
-  `;
-
-  const ProfileImageContainer = styled.div`
-    margin-left: 1.5rem;
-  `;
-
-  const ProfileInfo = styled.div`
-    margin-left: 0;
-    display: flex;
-    flex-direction: column;
-  `;
-
-  const ProfileImage = styled.img`
-    height: 2.5rem;
-    width: 2.5rem;
-    border-radius: 100%;
-  `;
-
-  const ProfileName = styled.h4`
-    margin: 0.7rem;
-    margin-top: 0;
-    margin-bottom: 0;
-  `;
-
-  const Privacy = styled.h4`
-    margin: 0.7rem;
-    color: #0063b8;
-    margin-top: 0;
-    margin-bottom: 0;
-  `;
-
-  const inputRef = useRef<HTMLTextAreaElement>(null);
+ const inputRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     if (inputRef.current) {
@@ -117,7 +62,7 @@ const UserProfile: React.FC = () => {
     <>
       <ContainerHead>
         <CancelButton>Cancel</CancelButton>
-        <PostButton>Post</PostButton>
+        <PostButton onClick={handlePost}>Post</PostButton>
       </ContainerHead>
       <PersonContainer>
         <ProfileImageContainer>
@@ -129,20 +74,33 @@ const UserProfile: React.FC = () => {
         </ProfileInfo>
       </PersonContainer>
       <InputContainer>
-        <InputTextarea
+      <InputTitle
           ref={inputRef}
-          placeholder="Digite seu texto..."
+          placeholder="Digite seu título aqui..."
           rows={1}
           onChange={(e) => {
             const textarea = e.target as HTMLTextAreaElement;
             textarea.style.height = 'auto';
-            textarea.style.height = `${textarea.scrollHeight}px`;
+            textarea.style.height = `${textarea.scrollHeight} px`;
           }}
         />
+
+        <InputTextarea
+          ref={inputRef}
+          placeholder="Digite seu texto..."
+          rows={7}
+          onChange={(e) => {
+            const textarea = e.target as HTMLTextAreaElement;
+            textarea.style.height = 'auto';
+            textarea.style.height = `${textarea.scrollHeight} px`;
+          }}
+        />
+        
+        <TagsInput selectedTags={selectedTags} />
+
       </InputContainer>
     </>
   );
 };
 
-export default UserProfile;
-
+export default PostContent;
