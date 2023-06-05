803a9f7120005cf954c579ec0d49aa701f137a24
raissa2861 <raissa.sabino@sou.inteli.edu.br>
2023-06-05 15:24:21 -0300

    Fix: tentativas de backend

--- codigo/backend/src/Saved/saved.controller.ts
+++ codigo/backend/src/Saved/saved.controller.ts
@@ -7,28 +7,8 @@ import { UpdateSavedDto } from './dto/update-saved.dto';
 export class SavedController {
   constructor(private readonly savedService: SavedService) {}
 
-  @Post()
-  create(@Body() createSavedDto: CreateSavedDto) {
-    return this.savedService.create(createSavedDto);
-  }
-
   @Get()
   findAll() {
-    return this.savedService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.savedService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSavedDto: UpdateSavedDto) {
-    return this.savedService.update(+id, updateSavedDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.savedService.remove(+id);
+    return this.savedService.getAllLikeds();
   }
 }
--- codigo/backend/src/Saved/saved.service.ts
+++ codigo/backend/src/Saved/saved.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSavedDto } from './dto/create-saved.dto';
 import { UpdateSavedDto } from './dto/update-saved.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SavedService {
-  create(createSavedDto: CreateSavedDto) {
-    return 'This action adds a new saved';
+  constructor (private readonly prisma: PrismaService){}
+  
+  async getAllLikeds(){
+    return await this.prisma.likes.findMany()
   }
-
-  findAll() {
-    return `This action returns all saved`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} saved`;
-  }
-
-  update(id: number, updateSavedDto: UpdateSavedDto) {
-    return `This action updates a #${id} saved`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} saved`;
-  }
+  
 }
